1befcbe4d25c9504c3e97dbe8d3a45433450c4ac
bit24 <eric.ca.zhang@gmail.com>
2016-11-01 17:45:07 -0700

    Update prime3.java

--- training/prime3.java
+++ training/prime3.java
@@ -22,17 +22,21 @@ public class prime3 {
 	void execute() throws IOException {
 		input();
 		computeLegal();
+		fillPositions();
 		search(1);
 
 		PrintWriter printer = new PrintWriter(new BufferedWriter(new FileWriter("prime3.out")));
 		String[] solutionsInS = new String[solutions.size()];
 
-		for (int i = 0; i < solutions.size(); i++) {
+		for (int cIndex = 0; cIndex < solutions.size(); cIndex++) {
 			StringBuilder str = new StringBuilder();
-			for (int cDigit : solutions.get(i)) {
-				str.append(cDigit);
+
+			for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < 5; j++) {
+					str.append(solutions.get(cIndex)[i][j]);
+				}
 			}
-			solutionsInS[i] = str.toString();
+			solutionsInS[cIndex] = str.toString();
 		}
 		Arrays.sort(solutionsInS);
 
@@ -61,7 +65,6 @@ public class prime3 {
 
 	boolean[] isPrime = new boolean[100_000];
 	boolean[] isLegal = new boolean[100_000];
-	boolean[] rLegal = new boolean[100_000];
 
 	int digitsSum(int input) {
 		int digitsSum = 0;
@@ -82,9 +85,7 @@ public class prime3 {
 				if (cNum >= 10_000 && digitsSum(cNum) == requiredSum) {
 					for (int numDigits = 1; numDigits <= 10_000; numDigits *= 10) {
 						isLegal[cNum / numDigits] = true;
-						rLegal[cNum % numDigits] = true;
 					}
-					rLegal[cNum] = true;
 				}
 				for (int multiple = 2 * cNum; multiple < 100_000; multiple += cNum) {
 					isPrime[multiple] = false;
@@ -99,85 +100,183 @@ public class prime3 {
 		BufferedReader reader = new BufferedReader(new FileReader("prime3.in"));
 		StringTokenizer inputData = new StringTokenizer(reader.readLine());
 		requiredSum = Integer.parseInt(inputData.nextToken());
-		cGrid[0] = Integer.parseInt(inputData.nextToken());
+		cGrid[0][0] = Integer.parseInt(inputData.nextToken());
 		reader.close();
 	}
 
-	ArrayList<int[]> solutions = new ArrayList<int[]>();
-	// ArrayList<int[][]> solutions1 = new ArrayList<int[][]>();
+	ArrayList<int[][]> solutions = new ArrayList<int[][]>();
 
-	int[] cGrid = new int[25];
-	/*
-	 * int[][] cGrid1 = new int[5][5];
-	 * 
-	 * void search(int i, int j) { posLoop: for (int posValue = 0; posValue < 10; posValue++) { cGrid1[i][j] = posValue;
-	 * int cNum = 0; for (int newJ = 0; newJ <= j; newJ++) { cNum *= 10; cNum += cGrid1[i][newJ]; } if (!isLegal[cNum])
-	 * { continue posLoop; }
-	 * 
-	 * cNum = 0; for (int newI = 0; newI <= i; newI++) { cNum *= 10; cNum += cGrid1[newI][j]; } if (!isLegal[cNum]) {
-	 * continue posLoop; }
-	 * 
-	 * if (i == 4 && j == 0) { cNum = cGrid1[4][0] * 10_000 + cGrid1[3][1] * 1_000 + cGrid1[2][2] * 100 + cGrid1[1][3] *
-	 * 10 + cGrid1[0][4]; if (!isLegal[cNum]) { continue posLoop; } }
-	 * 
-	 * if (i == j) { cNum = 0; for (int newIJ = 0; newIJ <= i; newIJ++) { cNum *= 10; cNum += cGrid1[newIJ][newIJ]; } if
-	 * (!isLegal[cNum]) { continue posLoop; } }
-	 * 
-	 * if (i == 4 && j == 4) { int[][] copy = new int[5][5]; for (int tempI = 0; tempI < 5; tempI++) { for (int tempJ =
-	 * 0; tempJ < 5; tempJ++) { copy[tempI][tempJ] = cGrid1[tempI][tempJ]; } } solutions1.add(copy); } else { if (j ==
-	 * 4) { search(i + 1, 0); } else { search(i, j + 1); } } } }
-	 */
+	int[][] cGrid = new int[5][5];
+
+	// start with diagonal 1
+	// continue with diagonal 2
+	// fill first row
+	// fill columns 2 and 4
+	// fill last row
+	// fill column 3
+	// fill rows 2, 3, and 4
+
+	int DIAGONAL_TB = 1;
+	int DIAGONAL_BT = 2;
+	int ROW = 3;
+	int COLUMN = 4;
+
+	int[] iIndices = new int[25];
+	int[] jIndices = new int[25];
+	boolean[][] filled;
+
+	void fillPositions() {
+		filled = new boolean[5][5];
+
+		filled[0][0] = true;
+		int count = 0;
+
+		for (int temp = 0; temp < 5; temp++) {
+			iIndices[count] = temp;
+			jIndices[count++] = temp;
+			filled[temp][temp] = true;
+		}
+
+		for (int temp = 4; temp >= 0; temp--) {
+			if (!filled[temp][4 - temp]) {
+				iIndices[count] = temp;
+				jIndices[count++] = 4 - temp;
+				filled[temp][4 - temp] = true;
+			}
+		}
+
+		count = fillRow(0, count);
+		count = fillColumn(1, count);
+		count = fillColumn(3, count);
+		count = fillRow(4, count);
+		count = fillColumn(2, count);
+		count = fillRow(1, count);
+		count = fillRow(2, count);
+		count = fillRow(3, count);
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				filled[i][j] = false;
+			}
+		}
+		filled[0][0] = true;
+	}
+
+	int fillRow(int rowNumber, int count) {
+		for (int temp = 0; temp < 5; temp++) {
+			if (!filled[rowNumber][temp]) {
+				iIndices[count] = rowNumber;
+				jIndices[count++] = temp;
+				filled[rowNumber][temp] = true;
+			}
+		}
+		return count;
+	}
+
+	int fillColumn(int columnNumber, int count) {
+		for (int temp = 0; temp < 5; temp++) {
+			if (!filled[temp][columnNumber]) {
+				iIndices[count] = temp;
+				jIndices[count++] = columnNumber;
+				filled[temp][columnNumber] = true;
+			}
+		}
+		return count;
+	}
 
 	void search(int index) {
+		/*if(index == 12){
+			System.out.println("Close");
+		}*/
+		
+		int iIndex = iIndices[index];
+		int jIndex = jIndices[index];
+
 		posLoop: for (int posValue = 0; posValue < 10; posValue++) {
-			cGrid[index] = posValue;
+			if ((iIndex == 0 || jIndex == 0) && posValue == 0) {
+				continue;
+			}
+
+			cGrid[iIndex][jIndex] = posValue;
+			filled[iIndex][jIndex] = true;
+
 			int cNum = 0;
-			for (int i = (index / 5) * 5; i <= index; i++) {
-				cNum *= 10;
-				cNum += cGrid[i];
-			}
-			if (!isLegal[cNum]) {
-				continue posLoop;
-			}
 
-			cNum = 0;
-			for (int i = index % 5; i <= index; i += 5) {
-				cNum *= 10;
-				cNum += cGrid[i];
-			}
-			if (!isLegal[cNum]) {
-				continue posLoop;
-			}
-
-			if (index % 4 == 0 && index != 0 && index != 24) {
-				cNum = 0;
-				for (int i = index; i >= 4; i -= 4) {
+			// DIAGONAL TB
+			if (iIndex == jIndex) {
+				for (int temp = 0; temp < 5 && filled[temp][temp]; temp++) {
 					cNum *= 10;
-					cNum += cGrid[i];
+					cNum += cGrid[temp][temp];
+					if (!isLegal[cNum]) {
+						continue posLoop;
+					}
 				}
-				if (!rLegal[cNum]) {
-					continue posLoop;
-				}
-			}
-
-			if (index % 6 == 0) {
 				cNum = 0;
-				for (int i = 0; i <= index; i += 6) {
+
+				// DIAGONAL BT
+			} else if (iIndex + jIndex == 4) {
+				for (int temp = 4; 0 <= temp && filled[temp][4 - temp]; temp--) {
 					cNum *= 10;
-					cNum += cGrid[i];
+					cNum += cGrid[temp][4 - temp];
+					if (!isLegal[cNum]) {
+						continue posLoop;
+					}
 				}
+				cNum = 0;
+			}
+			// ROWS
+			for (int temp = 0; temp < 5 && filled[iIndex][temp]; temp++) {
+				cNum *= 10;
+				cNum += cGrid[iIndex][temp];
 				if (!isLegal[cNum]) {
 					continue posLoop;
 				}
 			}
+			cNum = 0;
 
-			if (index == 24) {
-				solutions.add(Arrays.copyOf(cGrid, 25));
-			} else {
-				search(index + 1);
+			// COLUMNS
+			for (int temp = 0; temp < 5 && filled[temp][jIndex]; temp++) {
+				cNum *= 10;
+				cNum += cGrid[temp][jIndex];
+				if (!isLegal[cNum]) {
+					continue posLoop;
+				}
 			}
+			cNum = 0;
+
+			// all OK
+
+			if (index != 24) {
+				search(index + 1);
+			} else {
+				cNum = 0;
+				for (int temp = 0; temp < 5; temp++) {
+					cNum *= 10;
+					cNum += cGrid[iIndex][temp];
+				}
+				if (!isLegal[cNum]) {
+					continue posLoop;
+				}
+				cNum = 0;
+
+				for (int temp = 0; temp < 5; temp++) {
+					cNum *= 10;
+					cNum += cGrid[temp][jIndex];
+				}
+				if (!isLegal[cNum]) {
+					continue posLoop;
+				}
+
+				int[][] copy = new int[5][5];
+				for (int i = 0; i < 5; i++) {
+					for (int j = 0; j < 5; j++) {
+						copy[i][j] = cGrid[i][j];
+					}
+				}
+				solutions.add(copy);
+			}
+
 		}
+		filled[iIndex][jIndex] = false;
 	}
-	
-	
 }
