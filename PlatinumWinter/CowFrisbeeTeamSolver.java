3b10ae6d8366e5db21a7b89c9e1b60ba5930ac49
bit24 <eric.ca.zhang@gmail.com>
2016-12-19 15:33:43 -0800

    Update and rename CowFrisbeeTeam.java to CowFrisbeeTeamSolver.java

--- PlatinumWinter/CowFrisbeeTeamSolver.java
+++ PlatinumWinter/CowFrisbeeTeamSolver.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class CowFrisbeeTeam {
+public class CowFrisbeeTeamSolver {
 
 	static int modReq;
 
