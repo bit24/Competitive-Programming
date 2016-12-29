1201142706982c6aaff58d933c0639f6ae25097e
bit24 <eric.ca.zhang@gmail.com>
2016-12-28 16:57:37 -0800

    Update and rename TheValuesYouCanMake.java to Div2_361A.java

--- Codeforces/Div2_361A.java
+++ Codeforces/Div2_361A.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class TheValuesYouCanMake {
+public class Div2_361A {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
