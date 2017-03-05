d79d896a034c34f33a34538e777f968933e15b8f
bit24 <eric.ca.zhang@gmail.com>
2017-03-05 05:00:36 -0800

    Update and rename Div2_760A.java to Div2_393A.java

--- Codeforces/Div2_393A.java
+++ Codeforces/Div2_393A.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Div2_760A {
+public class Div2_393A {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
