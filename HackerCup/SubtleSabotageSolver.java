4c113f15b9e4e16e2756c2b4130c37d467ca31bf
bit24 <eric.ca.zhang@gmail.com>
2017-10-25 16:55:38 -0700

    Update and rename SubtleSabatogeSolver.java to SubtleSabotageSolver.java

--- HackerCup/SubtleSabotageSolver.java
+++ HackerCup/SubtleSabotageSolver.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-public class SubtleSabatogeSolver {
+public class SubtleSabotageSolver {
 
 	public static void main(String[] args) throws IOException {
 
