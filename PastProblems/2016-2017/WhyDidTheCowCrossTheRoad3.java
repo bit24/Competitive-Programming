fe463f1f463c9c96e7116d402cc707029f7738c8
bit24 <eric.ca.zhang@gmail.com>
2017-11-26 17:04:46 -0800

    Update and rename FriendCrossSolver.java to WhyDidTheCowCrossTheRoad3.java

--- PastProblems/2016-2017/WhyDidTheCowCrossTheRoad3.java
+++ PastProblems/2016-2017/WhyDidTheCowCrossTheRoad3.java
@@ -7,10 +7,10 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class FriendCrossSolver {
+public class WhyDidTheCowCrossTheRoad3 {
 
 	public static void main(String[] args) throws IOException {
-		new FriendCrossSolver().execute();
+		new WhyDidTheCowCrossTheRoad3().execute();
 	}
 
 	int numE;
