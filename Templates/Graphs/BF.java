bc86f208a751dee11d18a0639eac15a1b6d0cd89
bit24 <eric.ca.zhang@gmail.com>
2018-03-21 22:17:39 -0700

    Update BF.java

--- Templates/Graphs/BF.java
+++ Templates/Graphs/BF.java
@@ -2,43 +2,44 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+// runtime: O(nV * nE) but can handle negative edge weights
+
 class BF {
 
 	ArrayList<Edge> edges = new ArrayList<Edge>();
 	int[] distance;
-	int numVertices;
+	int nV;
 
 	public void bellmanFord(int source) {
-		distance = new int[numVertices];
+		distance = new int[nV];
 		Arrays.fill(distance, Integer.MAX_VALUE / 4);
 
 		distance[source] = 0;
 		Collections.sort(edges);
 
-		for (int i = 0; i < numVertices - 1; i++) {
+		for (int i = 0; i < nV - 1; i++) {
 			for (Edge e : edges) {
-				if (distance[e.endPointA] != Integer.MAX_VALUE / 4) {
-					distance[e.endPointB] = Math.min(distance[e.endPointB], distance[e.endPointA] + e.length);
+				if (distance[e.v1] != Integer.MAX_VALUE / 4) {
+					distance[e.v2] = Math.min(distance[e.v2], distance[e.v1] + e.len);
 				}
 			}
 		}
+		// if edges continue to be relaxed after this, there is a negative cycle
 	}
 
 	class Edge implements Comparable<Edge> {
-		int endPointA;
-		int endPointB;
-
-		int length;
+		int v1;
+		int v2;
+		int len;
 
 		public int compareTo(Edge o) {
-			return Integer.compare(length, o.length);
+			return Integer.compare(len, o.len);
 		}
 
-		public Edge(int endPointA, int endPointB, int length) {
-			this.endPointA = endPointA;
-			this.endPointB = endPointB;
-			this.length = length;
+		public Edge(int v1, int v2, int len) {
+			this.v1 = v1;
+			this.v2 = v2;
+			this.len = len;
 		}
 	}
-
 }
