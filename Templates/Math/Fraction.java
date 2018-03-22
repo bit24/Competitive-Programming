0ab336c524cbac80194dfee0aa1cf627f2ead529
bit24 <eric.ca.zhang@gmail.com>
2018-03-21 22:47:29 -0700

    Update Fraction.java

--- Templates/Math/Fraction.java
+++ Templates/Math/Fraction.java
@@ -3,88 +3,61 @@ class Fraction {
 	long num;
 	long den;
 
-	public Fraction(long value) {
+	Fraction(long value) {
 		num = value;
 		den = 1;
-	};
+	}
 
 	public boolean equals(Object object) {
 		simplify();
 		Fraction other = (Fraction) object;
-
-		if (other == null) {
-			return false;
-		}
+		other.simplify();
 
 		return num == other.num && den == other.den;
 	}
 
-	public Fraction(long numerator, long denominator) {
-		this.num = numerator;
-		this.den = denominator;
+	Fraction(long num, long den) {
+		this.num = num;
+		this.den = den;
 		simplify();
 	}
 
-	public Fraction multiply(Fraction other) {
+	Fraction multiply(Fraction other) {
 		return new Fraction(num * other.num, den * other.den);
 	}
 
-	public Fraction multiply(int other) {
-		return multiply(new Fraction(other));
-	}
-	////
-
-	public Fraction divide(Fraction other) {
+	Fraction divide(Fraction other) {
 		return new Fraction(num * other.den, den * other.num);
 	}
 
-	public Fraction divide(int other) {
-		return divide(new Fraction(other));
-	}
-	////
-
-	public Fraction add(Fraction other) {
-		long newNumerator = this.num * other.den + other.num * this.den;
-		long newDenominator = this.den * other.den;
-		return new Fraction(newNumerator, newDenominator);
+	Fraction add(Fraction other) {
+		return new Fraction(num * other.den + other.num * den, den * other.den);
 	}
 
-	public Fraction add(int other) {
-		return add(new Fraction(other));
-	}
-	////
-
-	public Fraction subtract(Fraction other) {
-		long newNumerator = this.num * other.den - other.num * this.den;
-		long newDenominator = this.den * other.den;
-		return new Fraction(newNumerator, newDenominator);
+	Fraction subtract(Fraction other) {
+		return new Fraction(num * other.den - other.num * den, den * other.den);
 	}
 
-	public Fraction subtract(int other) {
-		return subtract(new Fraction(other));
-	}
-	////
-
-	public void simplify() {
+	void simplify() {
 		assert (den != 0);
 		long gcd = gcd(num, den);
 		if (gcd == 0) {
-			return;
+			throw new RuntimeException();
 		}
 		num /= gcd;
 		den /= gcd;
 
 		if (den < 0) {
-			den *= -1;
-			num *= -1;
+			den = -den;
+			num = -num;
 		}
 	}
 
-	public double toInt() {
+	double toInt() {
 		return (num / den);
 	}
 
-	public long gcd(long a, long b) {
+	long gcd(long a, long b) {
 		if (b == 0) {
 			return a;
 		}
