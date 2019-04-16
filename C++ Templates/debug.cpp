d0b30b0b27c3a71dd55594beca0e3888d4e9955b
bit24 <eric.ca.zhang@gmail.com>
2019-04-15 18:21:00 -0700

    Create debug.cpp

---
+++ C++ Templates/debug.cpp
@@ -0,0 +1,56 @@
+namespace debug {
+    template<class T1, class T2>
+    void pr(const pair<T1, T2> &x);
+
+    template<class T, size_t SZ>
+    void pr(const array<T, SZ> &x);
+
+    template<class T>
+    void pr(const vector<T> &x);
+
+    template<class T>
+    void pr(const set<T> &x);
+
+    template<class T1, class T2>
+    void pr(const map<T1, T2> &x);
+
+    template<class T>
+    void pr(const T &x) { cout << x; }
+
+    template<class T, class... Ts>
+    void pr(const T &first, const Ts &... rest) { pr(first), pr(rest...); }
+
+    template<class T1, class T2>
+    void pr(const pair<T1, T2> &x) { pr("{", x.f, ", ", x.s, "}"); }
+
+    template<class T>
+    void prIn(const T &x) {
+        pr("{");
+        bool fst = 1;
+        for (auto &a : x) {
+            pr(fst ? "" : ", ", a), fst = 0;
+        }
+        pr("}");
+    }
+
+    template<class T, size_t SZ>
+    void pr(const array<T, SZ> &x) { prIn(x); }
+
+    template<class T>
+    void pr(const vector<T> &x) { prIn(x); }
+
+    template<class T>
+    void pr(const set<T> &x) { prIn(x); }
+
+    template<class T1, class T2>
+    void pr(const map<T1, T2> &x) { prIn(x); }
+
+
+    void ps() { pr("\n"); }
+
+    template<class Arg, class... Args>
+    void ps(const Arg &first, const Args &... rest) {
+        pr(first, " ");
+        ps(rest...); // print w/ spaces
+    }
+}
