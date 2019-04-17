3a47ddc9895a3fa266d290e5167cd4fef44ce522
bit24 <eric.ca.zhang@gmail.com>
2019-04-16 20:33:07 -0700

    Create lca.cpp

---
+++ C++ Templates/lca.cpp
@@ -0,0 +1,39 @@
+namespace lca {
+    const int LOGN = 18;
+
+    int anc[MAXN][LOGN];
+    int depth[MAXN];
+
+    void prep() {
+        for (int k = 1; k < LOGN; k++) {
+            for (int i = 0; i < N; i++) {
+                anc[i][k] = anc[anc[i][k - 1]][k - 1];
+            }
+        }
+    }
+
+    int j(int a, int d) {
+        for (int i = 0; i < LOGN; i++) {
+            if (d & (1 << i)) {
+                a = anc[a][i];
+            }
+        }
+        return a;
+    }
+
+    int fLCA(int a, int b) {
+        if (depth[a] > depth[b]) swap(a, b);
+
+        b = j(b, depth[b] - depth[a]);
+        
+        if (a == b) return a;
+
+        for (int i = LOGN - 1; i >= 0; i--) {
+            if (anc[a][i] != anc[b][i]) {
+                a = anc[a][i];
+                b = anc[b][i];
+            }
+        }
+        return anc[a][0];
+    }
+};
