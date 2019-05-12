2fa27da627deb0ff56d54e6789d818dc56e4b6a8
bit24 <eric.ca.zhang@gmail.com>
2019-05-12 09:27:56 -0700

    Add files via upload

---
+++ Contests/Codeforces C++/1163A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+
+#define pb push_back
+#define f first
+#define s second
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    cout << min(n - m, max(1, m));
+}
