2ed3a3f5763e55fc423303efb4213579b363de5a
bit24 <eric.ca.zhang@gmail.com>
2020-01-01 13:47:43 -0800

    Create hash.cpp

---
+++ C++ Templates/hash.cpp
@@ -0,0 +1,24 @@
+namespace Hash {
+    const ll MOD = 1e9 + 7;
+    const ll BASE = 30689;
+    ll POW[MAXN];
+
+    void cPow() {
+        POW[0] = 1;
+        for (int i = 1; i < MAXN; i++) {
+            POW[i] = POW[i - 1] * BASE % MOD;
+        }
+    }
+
+    void cPreH(string &str, ll preH[]) {
+        preH[0] = 0;
+        for (int i = 1; i < str.length(); i++) {
+            preH[i] = (preH[i - 1] * BASE + str[i]) % MOD;
+        }
+    }
+
+    ll cHash(ll preH[], int s, int e) {
+        return (preH[e] - preH[s - 1] * POW[e - s + 1] % MOD + MOD) % MOD;
+    }
+}
+using namespace Hash;
