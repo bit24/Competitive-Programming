0c2a0d386b5860d104ed6e257fec38adc5b2fa07
bit24 <eric.ca.zhang@gmail.com>
2020-03-17 17:53:27 -0700

    Update lca.cpp

--- C++ Templates/lca.cpp
+++ C++ Templates/lca.cpp
@@ -3,8 +3,21 @@ struct LCA {
 
     int anc[MAXN][LOGN];
     int depth[MAXN];
+    
+    void dfs(int cV, int pV) {
+       for (int aV : aL[cV]) {
+           if (aV != pV) {
+               depth[aV] = depth[cV] + 1;
+               dfs(aV, cV);
+               anc[aV][0] = cV;
+           }
+       }
+    }
 
     void prep() {
+        depth[0] = 0; // if root is different change
+        dfs(0, -1);
+        anc[0][0] = 0;
         for (int k = 1; k < LOGN; k++) {
             for (int i = 0; i < N; i++) {
                 anc[i][k] = anc[anc[i][k - 1]][k - 1];
